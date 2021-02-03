2a60679efdf4ef0dcae09c1b42cb9ff52f0cc2f6
bardin08 <asdasfb326@gmail.com>
2021-02-03 17:06:27 +0200

    Update database

--- BlindDateBot.Data/Configurations/UsersConfiguration.cs
+++ BlindDateBot.Data/Configurations/UsersConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using BlindDateBot.Domain.Models;
+﻿
+using BlindDateBot.Domain.Models;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -10,11 +11,10 @@ namespace BlindDateBot.Data.Configurations
         public void Configure(EntityTypeBuilder<UserModel> builder)
         {
             builder.HasKey(e => e.Id).HasName("user_pkey");
-            builder.Property(e => e.Id).HasColumnName("user_id").UseIdentityColumn();
+            builder.Property(e => e.Id).HasColumnName("user_id");
             builder.Property(e => e.Username).HasColumnName("username");
-            builder.Property(e => e.TelegramId).HasColumnName("username_telegram_id");
             builder.Property(e => e.Gender).HasColumnName("user_gender");
-            builder.Property(e => e.InterlocutorGender).HasColumnName("interlocuter_gender");
+            builder.Property(e => e.InterlocutorGender).HasColumnName("interlocutor_gender");
             builder.Property(e => e.IsFree).HasColumnName("is_free");
         }
     }
---
+++ BlindDateBot.Data/Migrations/20210203133145_CreateDb.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlindDateBot.Data.Migrations
+{
+    public partial class CreateDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TelegramId = table.Column<int>(type: "int", nullable: false),
+                    username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    user_gender = table.Column<int>(type: "int", nullable: false),
+                    interlocutor_gender = table.Column<int>(type: "int", nullable: false),
+                    is_free = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("user_pkey", x => x.user_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dates",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstUserId = table.Column<int>(type: "int", nullable: true),
+                    SecondUserId = table.Column<int>(type: "int", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dates_Users_FirstUserId",
+                        column: x => x.FirstUserId,
+                        principalTable: "Users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Dates_Users_SecondUserId",
+                        column: x => x.SecondUserId,
+                        principalTable: "Users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FromId = table.Column<int>(type: "int", nullable: true),
+                    ToId = table.Column<int>(type: "int", nullable: true),
+                    DateModelId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Dates_DateModelId",
+                        column: x => x.DateModelId,
+                        principalTable: "Dates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_FromId",
+                        column: x => x.FromId,
+                        principalTable: "Users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_ToId",
+                        column: x => x.ToId,
+                        principalTable: "Users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dates_FirstUserId",
+                table: "Dates",
+                column: "FirstUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dates_SecondUserId",
+                table: "Dates",
+                column: "SecondUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_DateModelId",
+                table: "Messages",
+                column: "DateModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_FromId",
+                table: "Messages",
+                column: "FromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ToId",
+                table: "Messages",
+                column: "ToId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Dates");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
