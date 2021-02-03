1c49b03e8e16fa4e3b726a809ac01389012ef752
bardin08 <asdasfb326@gmail.com>
2021-02-03 17:08:23 +0200

    Add date and message models

---
+++ BlindDateBot.Domain/Models/DateModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace BlindDateBot.Domain.Models
+{
+    public class DateModel
+    {
+        public DateModel(string id)
+        {
+            Id = id;
+            Messages = new();
+        }
+
+        public string Id { get; set; }
+        public UserModel FirstUser { get; set; }
+        public UserModel SecondUser { get; set; }
+        public bool IsActive { get; set; }
+        public List<MessageModel> Messages { get; set; }
+    }
+}
