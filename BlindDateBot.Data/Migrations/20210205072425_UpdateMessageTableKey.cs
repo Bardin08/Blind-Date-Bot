cfaa02b72dd97ba3e9a89be549004f0e2ca4da69
bardin08 <asdasfb326@gmail.com>
2021-02-08 12:54:53 +0200

    Update database

---
+++ BlindDateBot.Data/Migrations/20210205072425_UpdateMessageTableKey.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlindDateBot.Data.Migrations
+{
+    public partial class UpdateMessageTableKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
